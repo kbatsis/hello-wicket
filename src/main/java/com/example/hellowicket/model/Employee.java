ccb46c9516bee8fc738b38cc0238d020342ae256
01solutions\kbatsis <konstantinos.batsis@01solutions.gr>
2026-03-10 12:23:57 +0200

    H2, model & repository added.

---
+++ src/main/java/com/example/hellowicket/model/Employee.java
@@ -0,0 +1,53 @@
+package com.example.hellowicket.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "FirstName", length = 30, nullable = false)
+    private String firstName;
+
+    @Column(name = "LastName", length = 30, nullable = false)
+    private String lastName;
+
+    public Employee() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/hellowicket/repository/EmployeeRepository.java
@@ -0,0 +1,4 @@
+package com.example.hellowicket.repository;
+
+public interface EmployeeRepository {
+}
