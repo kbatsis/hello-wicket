bb3ee1f49aa23c1bdace8d9c69136e687d8f04c4
01solutions\kbatsis <konstantinos.batsis@01solutions.gr>
2026-03-17 09:12:21 +0200

    Removed service injection.

--- src/main/java/com/example/hellowicket/SupervisorDropDown.java
+++ src/main/java/com/example/hellowicket/SupervisorDropDown.java
@@ -1,16 +1,11 @@
 package com.example.hellowicket;
 
-import com.example.hellowicket.service.IEmployeeService;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import java.util.List;
 
 public class SupervisorDropDown extends DropDownChoice<Supervisor> {
-    @SpringBean
-    private IEmployeeService employeeService;
-
     public SupervisorDropDown(String id, List<? extends Supervisor> choices) {
         super(id, choices, new ChoiceRenderer<>() {
             @Override
